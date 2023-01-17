d5f4abda60d87e30898de0bbb14a5bb7ff8353f3
SeoMiYoung <seo20seo00@naver.com>
2023-01-17 23:33:40 +0900

    xhrData를 순서 상관없는 객체로 인수 전달하기

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -9,12 +9,11 @@
   */
 
 
-// xhrData 함수 만들기 method, url 
+// xhrData 함수 만들기 method, url, body 
 function xhrData(method,url,body){
-
+  // 둘이 세트임
   const xhr = new XMLHttpRequest();
-  // 비동기 통신 오픈
-  xhr.open(method,url);
+  xhr.open(method,url); // 비동기 통신 오픈
 
   // 객체 구조 분해 할당 -> xhr은 객체이므로 가능
   // xhr이 객체인데 우리는 xhr.status, xhr.readyState, xhr.response이렇게 객체의 것을 꺼내서 쓰고 있어 -> 번거롭다 이거지!
@@ -35,29 +34,38 @@ function xhrData(method,url,body){
   xhr.send(JSON.stringify(body));
 }
 
-
-xhrData('POST','https://jsonplaceholder.typicode.com/users', {
-  "name": "Mi Young Seo",
-  "username": "Ming",
-  "email": "seo20seo00@naver.com",
-  "address": {
-    "street": "Kulas Light",
-    "suite": "Apt. 556",
-    "city": "Gwenborough",
-    "zipcode": "92998-3874",
-    "geo": {
-      "lat": "-37.3159",
-      "lng": "81.1496"
-    }
-  },
-  "phone": "1-770-736-8031 x56442",
-  "website": "hildegard.org",
-  "company": {
-    "name": "Romaguera-Crona",
-    "catchPhrase": "Multi-layered client-server neural-net",
-    "bs": "harness real-time e-markets"
+// 순서 상관없는 객체를 넘겨주겠다
+xhrData({
+  url: 'https://jsonplaceholder.typicode.com/users',
+  method: 'GET',
+  body: null,
+  headers: {
+    'Content-Type': 'application/json'
   }
 });
+
+// xhrData('POST','https://jsonplaceholder.typicode.com/users', {
+//   "name": "Mi Young Seo",
+//   "username": "Ming",
+//   "email": "seo20seo00@naver.com",
+//   "address": {
+//     "street": "Kulas Light",
+//     "suite": "Apt. 556",
+//     "city": "Gwenborough",
+//     "zipcode": "92998-3874",
+//     "geo": {
+//       "lat": "-37.3159",
+//       "lng": "81.1496"
+//     }
+//   },
+//   "phone": "1-770-736-8031 x56442",
+//   "website": "hildegard.org",
+//   "company": {
+//     "name": "Romaguera-Crona",
+//     "catchPhrase": "Multi-layered client-server neural-net",
+//     "bs": "harness real-time e-markets"
+//   }
+// });
 
 
 
