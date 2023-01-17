e5e9bf725c9984bac05bb8d8ec95b6b7c2ee3be4
SeoMiYoung <seo20seo00@naver.com>
2023-01-17 23:50:21 +0900

    객체의 초기값 세팅

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -10,10 +10,14 @@
 
 
 // xhrData 함수 만들기 method, url, body 
-function xhrData(options){ // option대신 {method, url, body}가능
-  // 객체의 구조 분해 할당 -> 장점: 초기값 설정 가능
-  const { method='GET', url='', body=null } = options;
-
+function xhrData({ 
+  method='GET', 
+  url='', 
+  body=null, 
+  headers={
+    'Content-Type': 'application/json'
+  }
+}){ 
   // 둘이 세트임
   const xhr = new XMLHttpRequest();
   xhr.open(method,url); // 비동기 통신 오픈
@@ -38,14 +42,7 @@ function xhrData(options){ // option대신 {method, url, body}가능
 }
 
 // 순서 상관없는 객체를 넘겨주겠다
-xhrData({
-  url: 'https://jsonplaceholder.typicode.com/users',
-  method: 'GET',
-  body: null,
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
+xhrData({url: 'https://jsonplaceholder.typicode.com/users'});
 
 // xhrData('POST','https://jsonplaceholder.typicode.com/users', {
 //   "name": "Mi Young Seo",
